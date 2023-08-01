e4cf4505c86a52521b836169b0b936a1fd1bcc94
yehezkieldio <yehezkieldio35@gmail.com>
2023-08-01 14:35:37 +0800

    feat: redo page-main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,32 +6,29 @@ export default function Main() {
     return (
         <main className="page-main">
             <div className="page-container">
-                <div className="main-content">
-                    <article className="page-content">
-                        <h1 className="content-title">I&apos;M YEHEZKIEL DIO</h1>
-                        <p className="content-description">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates inventore nulla illum
-                            deleniti sit placeat consequuntur rerum vero tenetur minus soluta ipsum ut nisi assumenda
-                            corrupti est laborum, ratione natus numquam molestiae optio et!
+                <div className="page-main-content">
+                    <article>
+                        <h1>I&apos;M YEHEZKIEL DIO</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet sapiente culpa pariatur,
+                            nam rem nostrum, distinctio, omnis placeat eligendi dignissimos labore magnam saepe
+                            exercitationem voluptate est aperiam sit!
                         </p>
                     </article>
-                    <div className="page-more-about-me">
-                        <Link className="call-to-action" href="/about">
-                            <p>
-                                More About Me
-                                <svg
-                                    width="114"
-                                    height="24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                >
-                                    <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"></path>
-                                </svg>
-                            </p>
+                    <div>
+                        <Link href="/about">
+                            More About Me{" "}
+                            <svg
+                                width="114"
+                                height="24"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                            >
+                                <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"></path>
+                            </svg>
                         </Link>
                     </div>
-                    <div className="page-socials"></div>
                 </div>
             </div>
         </main>
