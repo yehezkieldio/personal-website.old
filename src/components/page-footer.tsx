ad90b9def50cd3ab3428b78a4c0e42b584d8d08b
yehezkieldio <yehezkieldio35@gmail.com>
2023-08-01 14:19:55 +0800

    feat: page-footer

--- src/components/page-footer.tsx
+++ src/components/page-footer.tsx
@@ -5,7 +5,19 @@ export default function PageFooter() {
 
     return (
         <footer className="page-footer">
-            <div className="page-footer__inner">{currentYear}</div>
+            <div className="page-footer-content">
+                <p className="open-source">
+                    The source code for this site is available on{" "}
+                    <Link href="https://github.com/elizielx/eleutheria" target="_blank" rel="noreferrer nofollow">
+                        GitHub
+                    </Link>{" "}
+                    under the MIT License.
+                </p>
+                <p className="copyright">
+                    Copyright © {currentYear} <span className="highlight">Yehezkiel Dio Sinolungan</span>. All Rights
+                    Reserved.
+                </p>
+            </div>
         </footer>
     );
 }
