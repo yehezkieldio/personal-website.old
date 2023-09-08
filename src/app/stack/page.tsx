6a13f13e842e34f6d18001cd8ceb86bb2c1f6711
yehezkieldio <yehezkieldio35@gmail.com>
2023-09-09 05:42:12 +0800

    feat: tech data

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -7,12 +7,51 @@ export const metadata: Metadata = {
     description: "My technology stack, including languages, frameworks, and tools.",
 };
 
+const techData = [
+    {
+        tag: "languages",
+        technologies: ["html5", "css3", "sass", "javascript", "typescript", "go", "php"],
+    },
+    {
+        tag: "nodejs",
+        technologies: ["node.js", "react", "next.js", "vite", "nestjs", "express", "fastify", "electron"],
+    },
+    {
+        tag: "databases",
+        technologies: ["prisma", "mysql", "mariadb", "postgresql", "mongodb", "sqlite"],
+    },
+    {
+        tag: "others",
+        technologies: ["laravel", "flutter"],
+    },
+    {
+        tag: "message brokers/queues",
+        technologies: ["redis", "apachekafka", "rabbitmq"],
+    },
+    {
+        tag: "css frameworks",
+        technologies: ["bootstrap", "bulma", "tailwindcss", "chakraui"],
+    },
+    {
+        tag: "operating systems/containers",
+        technologies: ["windows", "archlinux", "docker"],
+    },
+    {
+        tag: "devops/build systems",
+        technologies: ["nx", "githubactions", "circleci"],
+    },
+    {
+        tag: "code editors",
+        technologies: ["visualstudiocode", "goland"],
+    },
+];
+
 export default function Stack() {
     return (
         <article className="container">
             <h1>Tech Stack</h1>
             <NavLine />
-            <TechStack />
+            <TechStack techData={techData} />
         </article>
     );
 }
--- src/components/tech-stack.tsx
+++ src/components/tech-stack.tsx
@@ -26,27 +26,19 @@ export const Technologies = ({ tag, technologies }: { tag: string; technologies:
     );
 };
 
-export const TechStack = () => {
+export const TechStack = ({
+    techData,
+}: {
+    techData: {
+        tag: string;
+        technologies: string[];
+    }[];
+}) => {
     return (
         <div className="stack-container">
-            <Technologies
-                tag="languages"
-                technologies={["html5", "css3", "sass", "javascript", "typescript", "go", "php"]}
-            />
-            <Technologies
-                tag="nodejs"
-                technologies={["node.js", "react", "next.js", "vite", "nestjs", "express", "fastify", "electron"]}
-            />
-            <Technologies
-                tag="databases"
-                technologies={["prisma", "mysql", "mariadb", "postgresql", "mongodb", "sqlite"]}
-            />
-            <Technologies tag="others" technologies={["laravel", "flutter"]} />
-            <Technologies tag="message brokers/queues" technologies={["redis", "apachekafka", "rabbitmq"]} />
-            <Technologies tag="css frameworks" technologies={["bootstrap", "bulma", "tailwindcss", "chakraui"]} />
-            <Technologies tag="operating systems/containers" technologies={["windows", "archlinux", "docker"]} />
-            <Technologies tag="devops/build systems" technologies={["nx", "githubactions", "circleci"]} />
-            <Technologies tag="code editors" technologies={["visualstudiocode", "goland"]} />
+            {techData.map((techItem, index) => (
+                <Technologies key={index} tag={techItem.tag} technologies={techItem.technologies} />
+            ))}
         </div>
     );
 };
